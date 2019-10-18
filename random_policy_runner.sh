6a03a2c89ae1a0d6d107eae9212ac6d3c27d2235
Enrique Noriega <enrique.noriega@github.com>
2019-10-17 20:49:20 -0700

    Added the random policy runner script

---
+++ random_policy_runner.sh
@@ -0,0 +1,12 @@
+SEEDS="1024 0 6754 9802 4729 100"
+PROBS="0 .25 .5 .75 1"
+
+for PROB in $PROBS
+do
+  for SEED in $SEEDS
+  do
+    OUTPUT="balanced_random_${PROB}_${SEED}.tsv"
+    sbt -Dconfig.file=application_deathstar.conf -Drandom.seed=$SEED -Dtesting.exploreBias=$PROB -Dtesting.statsDump=$OUTPUT "runMain org.ml4ai.learning.TestFR"
+
+  done
+done
