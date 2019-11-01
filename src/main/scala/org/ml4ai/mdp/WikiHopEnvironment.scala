9cb3b834738714dd77cc627f0186d50a5f043d90
Enrique Noriega <enrique.noriega@gmail.com>
2019-11-01 11:43:53 -0700

    squared the reward to make more dramatic the contribution of papers read

--- src/main/scala/org/ml4ai/mdp/WikiHopEnvironment.scala
+++ src/main/scala/org/ml4ai/mdp/WikiHopEnvironment.scala
@@ -197,8 +197,8 @@ class WikiHopEnvironment(val id:String, val start:String, val end:String, docume
 //
 //    val failureReward = - papersRead.size
 
-    val successReward = - papersRead.size
-    val failureReward = - papersRead.size
+    val successReward = - (papersRead.size^2)
+    val failureReward = - (papersRead.size^2)
 
     val livingReward = WHConfig.Environment.livingReward
     val sigmoidFactor = successReward*0.5 // TODO Parameterize the ratio
