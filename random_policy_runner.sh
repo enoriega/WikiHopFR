0355cab348e4874304b4853d744ef2db9c28199e
Enrique Noriega <enrique.noriega@github.com>
2020-01-20 15:19:35 -0700

    Changes to the benchmark runner

--- random_policy_runner.sh
+++ random_policy_runner.sh
@@ -1,9 +1,13 @@
 SEEDS="1024 0 6754 9802 4729 100"
-PROBS="0 .25 .5 .75 1"
+#SEEDS="1024"
+PROBS="0.0 .0625 .125 .1875 .25 .5 .75 1"
+#PROBS="1.0"
 
-for PROB in $PROBS
+for SEED in $SEEDS
 do
-  for SEED in $SEEDS
+  CASCADE_OUTPUT="cascade_${SEED}.tsv"
+  sbt -Dconfig.file=application_deathstar.conf -Drandom.seed="$SEED" -Dtesting.agentType=Cascade -Dtesting.statsDump="$CASCADE_OUTPUT" "runMain org.ml4ai.learning.TestFR"
+  for PROB in $PROBS
   do
     OUTPUT="balanced_random_${PROB}_${SEED}.tsv"
     sbt -Dconfig.file=application_deathstar.conf -Drandom.seed="$SEED" -Dtesting.exploreBias="$PROB" -Dtesting.statsDump="$OUTPUT" "runMain org.ml4ai.learning.TestFR"
