52c65241a07079760339669cb3950f4d6b2c6013
Enrique Noriega <enrique.noriega@gmail.com>
2020-06-20 11:22:36 -0400

    Building the GT graph

--- src/main/scala/org/ml4ai/utils/GroundTruthUpperBound.scala
+++ src/main/scala/org/ml4ai/utils/GroundTruthUpperBound.scala
@@ -1,48 +1,84 @@
 package org.ml4ai.utils
 
-import org.ml4ai.ie.WikificationEntities
+import org.clulab.utils.Serializer
+import org.ml4ai.ie.{WikificationEntities, WikificationEntry}
 import org.ml4ai.inference.{AttributingElement, KBLabel, Relation}
 import scalax.collection.Graph
+import scalax.collection.GraphEdge.UnDiEdge
 import scalax.collection.edge.LUnDiEdge
 import scalax.collection.edge.Implicits._
+import scalax.collection.GraphEdge._
+import scalax.collection.GraphPredef._
+
+import scala.collection.mutable
 
 object GroundTruthUpperBound extends App {
 
   // First read the wikification DB
+  println("Parsing input files")
   val wikificationEntities = WikificationEntities.readWikificationDataBase()
 
+  println(wikificationEntities.size)
+
   // Now put together the edges of entities
-  val edges =
-    wikificationEntities.par.flatMap{
-      case (docHash, docValues) =>
-      // Group by sentence index
-      val bySent = docValues.groupBy(e => e.sentence)
-      // Generate an "edge" (relation) per each of the different concepts that appear on the same line
-        bySent.flatMap{
-          case (senIx, entriesInLine) =>
-            for{
-              a <- entriesInLine.distinct
-              b <- entriesInLine.distinct
-              if a.wikiId != b.wikiId
-            } yield Relation(a.wikiId, b.wikiId, Seq(AttributingElement(None, senIx, docHash)))
+  println("Building the relations")
+
+  val groupedEntries =
+    wikificationEntities.keys.flatMap{
+      docId =>
+        var maxSent = 0
+        for(entry <- wikificationEntities(docId)){
+          if(entry.sentence > maxSent)
+            maxSent = entry.sentence
         }
+        (0 to maxSent) map (six => (docId, six) -> mutable.ListBuffer[WikificationEntry]())
+    }.toMap
+
+  println("Generating the edges")
+  for((docHash, entries) <- wikificationEntities.par){
+    for(entry <- entries){
+      val key = (docHash, entry.sentence)
+      groupedEntries(key) += entry
     }
+  }
 
-  // Cluster together the edges with the same endpoints and aggregate the attributions
-  val groupedEdges = edges.groupBy(r => (r.sourceHash, r.destinationHash)).mapValues{
-    rels =>
-      Relation(rels.head.sourceHash, rels.head.destinationHash, rels.flatMap(_.attributions).seq)
-  }.values
+  val entriesSize = groupedEntries.keys.toList.size
 
-  // How many edges are in the global graph?
-  println(groupedEdges.size)
+  val edges = new mutable.HashMap[(Int, Int), mutable.ListBuffer[AttributingElement]]()
+
+
+  var ix = 0
+  for(((docHash, senIx), entries) <- groupedEntries) {
+    if((ix + 1)% 10000 == 0){
+      println(s"Elem ${ix + 1} of $entriesSize")
+    }
+    val observed = mutable.HashSet[(Int, Int)]()
+    for{
+      a <- entries
+      b <- entries
+      if a.wikiId != b.wikiId
+    } {
+      val edgeKey = (a.wikiId, b.wikiId)
+      if(!observed.contains(edgeKey)){
+        if(!edges.contains(edgeKey)){
+          edges += edgeKey -> new mutable.ListBuffer[AttributingElement]
+        }
+        val attr = AttributingElement(None, senIx, docHash)
+        edges(edgeKey) += attr
+      }
+    }
+    ix += 1
+  }
 
   // Build THE global graph
   // Build graph
-  val graph: Graph[Int, LUnDiEdge] = Graph.from(edges = groupedEdges.map{
-    r =>
-      (r.sourceHash ~+ r.destinationHash)(KBLabel(r))
+  println("Building the Graph")
+  val graph: Graph[Int, UnDiEdge] = Graph.from(edges = edges.map{
+    case((source, dest), _) =>
+      source ~ dest
   }.seq)
 
   print(graph.size)
+
+  Serializer.save(graph, "gt_graph.ser")
 }
